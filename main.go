23eaaa4b539a42205720aae910d75970be256cf2
Sahas001 <tsahas2059@gmail.com>
2023-11-05 15:19:08 +0545

    feat: code update

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"net/http"
-	"text/template"
 )
 
 func main() {
@@ -11,7 +10,7 @@ func main() {
 	mux.Handle("/static/", http.StripPrefix("/static/", files))
 
 	mux.HandleFunc("/", index)
-	mux.HandleFunc("/err", err)
+//	mux.HandleFunc("/err", err)
 
 	mux.HandleFunc("/login", login)
 	mux.HandleFunc("/logout", logout)
@@ -19,10 +18,10 @@ func main() {
 	mux.HandleFunc("/signup_account", signupAccount)
 	mux.HandleFunc("/authenticate", authenticate)
 
-	mux.HandleFunc("/thread/new", newThread)
-	mux.HandleFunc("/thread/create", createThread)
-	mux.HandleFunc("/thread/post", postThread)
-	mux.HandleFunc("/thread/read", readThread)
+//	mux.HandleFunc("/thread/new", newThread)
+//	mux.HandleFunc("/thread/create", createThread)
+//	mux.HandleFunc("/thread/post", postThread)
+//	mux.HandleFunc("/thread/read", readThread)
 
 	server := &http.Server{
 		Addr:    "0.0.0.0:8080",
@@ -31,13 +30,4 @@ func main() {
 	server.ListenAndServe()
 }
 
-func index(w http.ResponseWriter, r *http.Request) {
-	files := []string{"templates/layout.html",
-		"templates/navbar.html",
-		"templates/index.html"}
-	templates := template.Must(template.ParseFiles(files...))
-	threads, err := data.Threads
-	if(err == nil) {
-		templates.ExecuteTemplate(w, "layout", threads)
-	}
-}
+
